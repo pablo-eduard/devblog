898cc7f06fe2a552267aeb33756f93bee8c5d6fa
EDUARDO AMROGINSKI SANTOS <03726138021@senacrs.edu.br>
2026-06-25 20:41:18 -0300

    Finalizando exercicio da aula 66

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sobre/', views.sobre_nos, name='sobre_nos')
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,8 @@ def home(request):
     mensagem = "<h1>Bem-vindo ao DevBlog!</h1> <p>Em breve, artigos aqui.</p>"
 
     return HttpResponse(mensagem)
+
+def sobre_nos(request):
+    mensagem = "<h1>Sobre o DevBlog!</h1>"
+
+    return HttpResponse(mensagem)
